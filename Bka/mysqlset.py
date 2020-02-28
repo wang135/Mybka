16b807d3297b30a4faa18a6af8a2e94a71050d8d
wang135 <31172613+wang135@users.noreply.github.com>
2020-02-28 15:49:27 +0800

    Update mysqlset.py

--- Bka/mysqlset.py
+++ Bka/mysqlset.py
@@ -5,11 +5,4 @@ import datetime
 class xiemysql:
 
 
-    host='172.30.16.214',
-
-    user='riskuser',
-    password ='1qaz@WSX',
-    db = 'HSBIData',
-    charset='utf8',
-
-# conn=pymssql.connect(host='172.30.16.214',user='riskuser',password='1qaz@WSX',database='HSBIData',charset='utf8')
+####数据缺少
